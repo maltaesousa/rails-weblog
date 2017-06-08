37f20dd975a900a4918bebbe41e14590c670f50c
Thibaud <thibaud@thibaud.gg>
2017-06-08 17:13:41 +0000

    exercice 1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#display_name when admin' do
+    bob = users(:bob)
+    assert_equal'Bob (admin)', bob.display_name
+  end
+  test '#display_name when not admin' do
+    jane = users(:jane)
+    assert_equal 'Jane', jane.display_name
+  end
 end
